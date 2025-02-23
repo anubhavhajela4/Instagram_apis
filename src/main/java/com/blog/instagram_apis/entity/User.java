1cfd8df6d23646fd3eefb57129fef4bf3ffabe62
Arth Kamboj <arth23153015@akgec.ac.in>
2025-02-23 14:10:11 +0530

    user entity class built

---
+++ src/main/java/com/blog/instagram_apis/entity/User.java
@@ -0,0 +1,22 @@
+package com.blog.instagram_apis.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private UUID id;
+    private String username;
+    private String fullname;
+    private String password;
+    private String email;
+    private String profilePicture;
+    private String bio;
+}
