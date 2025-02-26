f911e108bf5c3558d2ca7ad2521ac0a8774f385c
Arth Kamboj <arth23153015@akgec.ac.in>
2025-02-26 20:48:22 +0530

    Claim extraction methods setup

--- src/main/java/com/blog/instagram_apis/config/JwtAuthenticationFilter.java
+++ src/main/java/com/blog/instagram_apis/config/JwtAuthenticationFilter.java
@@ -5,10 +5,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
--- src/main/java/com/blog/instagram_apis/service/JwtService.java
+++ src/main/java/com/blog/instagram_apis/service/JwtService.java
@@ -1,4 +1,40 @@
 package com.blog.instagram_apis.service;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "8a06a7e9023f46466fac0d6d10e0422f67f4a09716c5fefa0a6c1157d8fb51bc";
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims= exctractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims exctractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build().parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
