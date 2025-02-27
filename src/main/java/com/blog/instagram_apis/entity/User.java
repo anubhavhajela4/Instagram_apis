0a7cb51b90bc6fb1afebaf8f2e47b5dfd55d2144
Arth Kamboj <arth23153015@akgec.ac.in>
2025-02-27 15:49:19 +0530

    Spring security established. Security and password encryption applied

--- src/main/java/com/blog/instagram_apis/entity/User.java
+++ src/main/java/com/blog/instagram_apis/entity/User.java
@@ -2,7 +2,11 @@ package com.blog.instagram_apis.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -10,7 +14,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,4 +26,39 @@ public class User {
     private String email;
     private String profilePicture;
     private String bio;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
